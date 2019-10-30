4091854e00a25c321d26c337a8b8b196fd99f76c
alaverma <alaverma@uoc.edu>
2019-10-30 07:31:12 +0100

    Creació de llibreria per fer scraping de la borsa

--- src/BolsaScraper_v1.py
+++ src/BolsaScraper_v1.py
@@ -3,32 +3,83 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-# Loads webpage
-
-# TODO - cercar l'empresa desitjada
+class BolsaScraper():
+    
+    def __init__(self):
+        # Definim l'url basica de la web
+        self.path = 'http://www.bolsamadrid.es'
+        
+    # TODO - cercar l'empresa desitjada
 #      - poder seleccionar les dates desitjades
+    def trobarEmpresa(self, nomEmpresa):
+        """
+        Aquesta funció retorna el path final de l'empresa que busquem
+        """
+        url = self.path + "/esp/aspx/Empresas/Empresas.aspx"
+        
+        page = requests.get(url)
+        
+        print(page)
+        
+    def dadesEmpresa(self):
+        """
+        Retorna les dades de l'empresa seleccionada
+        """
+        # Loads webpage -- Serà variable vindrà donada per la funció anterior
+        
+        url = 'http://www.bolsamadrid.es/esp/aspx/Empresas/InfHistorica.aspx?ISIN=ES0105200002'
+        
+        page = requests.get(url)
+        
+        soup = BeautifulSoup(page.content, 'html.parser')
+        
+        # Reads data
+        taula = soup.find(id="ctl00_Contenido_tblDatos")
+        
+        # Ticker
+        ticker = [soup.find(class_= 'FrmBusq').find_all('td')[10].string.strip()]
+        
+        # Company name
+        company = [soup.find(class_= 'FrmBusq').find_all('td')[8].string.strip()]
+        
+        # Heading row
+        first_row = [title.string for title in taula.find_all('th')]
+        
+        # Other rows
+        content = [[element.string for element in row.find_all('td')] for row in taula.find_all('tr')[1:]]
+        
+        
+        # Writes output csv file in 'output' dir with ticker company as filename
+        with open('output/'+ticker[0]+'.csv', 'w', newline='') as csvfile:
+            bolsa_writer = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            bolsa_writer.writerow(company)
+            bolsa_writer.writerow(first_row)
+            bolsa_writer.writerows(content)    
+        
+        
 
-page = requests.get('http://www.bolsamadrid.es/esp/aspx/Empresas/InfHistorica.aspx?ISIN=ES0105200002')
-soup = BeautifulSoup(page.content, 'html.parser')
+def main():
+    
+    bolsa = BolsaScraper()
+    
+    bolsa.dadesEmpresa()
+    
+if __name__ == "__main__":
+    main()
+    
 
-# Reads data
-taula = soup.find(id="ctl00_Contenido_tblDatos")
 
-# Ticker
-ticker = [soup.find(class_= 'FrmBusq').find_all('td')[10].string.strip()]
 
-# Company name
-company = [soup.find(class_= 'FrmBusq').find_all('td')[8].string.strip()]
 
-# Heading row
-first_row = [title.string for title in taula.find_all('th')]
 
-# Other rows
-content = [[element.string for element in row.find_all('td')] for row in taula.find_all('tr')[1:]]
 
-# Writes output csv file in 'output' dir with ticker company as filename
-with open('output/'+ticker[0]+'.csv', 'w', newline='') as csvfile:
-    bolsa_writer = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    bolsa_writer.writerow(company)
-    bolsa_writer.writerow(first_row)
-    bolsa_writer.writerows(content)    
+
+
+
+
+
+
+
+
+
+
