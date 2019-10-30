c4c13b91760bd2631bb88984bbfdf000df51d2b4
alaverma <alaverma@uoc.edu>
2019-10-30 07:52:14 +0100

    Funció per escreiure a csv i nova ubicació

--- src/BolsaScraper_v1.py
+++ src/BolsaScraper_v1.py
@@ -19,7 +19,12 @@ class BolsaScraper():
         
         page = requests.get(url)
         
-        print(page)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        
+        # Reads data
+        taula = soup.find(id="ctl00_Contenido_tblEmisoras")
+        
+        print(taula)
         
     def dadesEmpresa(self):
         """
@@ -27,7 +32,7 @@ class BolsaScraper():
         """
         # Loads webpage -- Serà variable vindrà donada per la funció anterior
         
-        url = 'http://www.bolsamadrid.es/esp/aspx/Empresas/InfHistorica.aspx?ISIN=ES0105200002'
+        url = self.path +'/esp/aspx/Empresas/InfHistorica.aspx?ISIN=ES0105200002'
         
         page = requests.get(url)
         
@@ -40,22 +45,23 @@ class BolsaScraper():
         ticker = [soup.find(class_= 'FrmBusq').find_all('td')[10].string.strip()]
         
         # Company name
-        company = [soup.find(class_= 'FrmBusq').find_all('td')[8].string.strip()]
+        self.company = [soup.find(class_= 'FrmBusq').find_all('td')[8].string.strip()]
         
         # Heading row
-        first_row = [title.string for title in taula.find_all('th')]
+        self.first_row = [title.string for title in taula.find_all('th')]
         
         # Other rows
-        content = [[element.string for element in row.find_all('td')] for row in taula.find_all('tr')[1:]]
+        self.content = [[element.string for element in row.find_all('td')] for row in taula.find_all('tr')[1:]]
         
         
-        # Writes output csv file in 'output' dir with ticker company as filename
-        with open('output/'+ticker[0]+'.csv', 'w', newline='') as csvfile:
+        self.data2csv(ticker[0])
+            
+    def data2csv(self, filename):
+        with open("../data/"+filename+'.csv', "w+", newline='') as csvfile:
             bolsa_writer = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            bolsa_writer.writerow(company)
-            bolsa_writer.writerow(first_row)
-            bolsa_writer.writerows(content)    
-        
+            bolsa_writer.writerow(self.company)
+            bolsa_writer.writerow(self.first_row)
+            bolsa_writer.writerows(self.content)
         
 
 def main():
@@ -63,6 +69,8 @@ def main():
     bolsa = BolsaScraper()
     
     bolsa.dadesEmpresa()
+    
+    bolsa.trobarEmpresa("ABENGOA, S.A.")
     
 if __name__ == "__main__":
     main()
