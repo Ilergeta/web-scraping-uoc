8c4fb5b827f7e5ed0928a8121745400e55e7a927
alaverma <alaverma@uoc.edu>
2019-11-11 10:37:35 +0100

    Nova forma d'execusió

--- src/main.py
+++ src/main.py
@@ -1,23 +1,39 @@
 # -*- coding: utf-8 -*f
+import argparse
+import sys
 from bolsaScraper import BolsaScraper
 
 scraper = BolsaScraper()
 
 
-# Write here companie or companies that you need do web scraping
-companies = ["SAN", "SABADELL"]
+parser = argparse.ArgumentParser()
 
-# Start date is manadatory argument
-start_date = "24/06/2019"
-# End date is optional argument if not use the scraper use today as end date
-end_date = "10/11/2019"
+parser.add_argument("--companies", nargs='+')
+parser.add_argument("--start")
+parser.add_argument("--end")
 
-for companie in companies:
-    # Obtained url
-    url = scraper.trobarEmpresa(companie)
-    # Scraping and saving informaiton about the companies
-    scraper.dadesEmpresa(url, start_date)
+known_args = parser.parse_known_args(sys.argv)
+
+for key, value in parser.parse_args()._get_kwargs():
+    if key == "companies":
+        companies = value
+    if key == "start":
+        start_date = value
+    if key == "end":
+        end_date = value
+print(companies, start_date, type(start_date), end_date)
+
+if end_date == None:
+    for companie in companies:
+        # Obtained url
+        url = scraper.trobarEmpresa(companie)
+        # Scraping and saving informaiton about the companies
+        scraper.dadesEmpresa(url, start_date)
+else:
+    for companie in companies:
+        # Obtained url
+        url = scraper.trobarEmpresa(companie)
+        # Scraping and saving informaiton about the companies
+        scraper.dadesEmpresa(url, start_date, end_date)
     
 
-# Aquí aniran totes les funcions amb l'estructura següent:
-# scraper.funció(parametres)
